ba18a574dcac7513eb3c4742401f687123b73da1
Maximilian Spendig <maximilian@spendig.de>
2025-12-29 16:45:20 +0100

    feat(maap): add MAC address range conflict detection and handle announce events
    - Introduce compare_mac() to detect overlapping MAC address allocations
    - Extend maap_state_machine to process RANNOUNCE events and handle conflicts by restarting allocation
    - Update announce and probe message counts to use CONFIG_TALKER_STREAM_SOURCES
    - Refactor state machine function signature to accept MAAP PDU messages

--- components/avb/maap.c
+++ components/avb/maap.c
@@ -2,6 +2,7 @@
 
 #include <avtp.h>
 #include <cc.h>
+#include <config.h>
 #include <esp_eth_spec.h>
 #include <esp_log.h>
 #include <esp_random.h>
@@ -11,7 +12,7 @@
 
 #define TAG "maap"
 
-void maap_state_machine(struct avtp_state_s* state, maap_event event);
+void maap_state_machine(struct avtp_state_s* state, maap_event event, struct maap_pdu_s* msg);
 
 void generate_address(u8* maap_mac)
 {
@@ -45,7 +46,7 @@ void maap_send_announce(struct avtp_state_s* state, const u8* allocated_mac, con
   create_maap_msg(state, &msg, MAAP_MSG_TYPE_ANNOUNCE);
 
   memcpy(msg.requested_start_address, state->maap_db.mac, ETH_ADDR_LEN);
-  msg.requested_count = htons(0x0001); // Announcing allocated MAC addresses
+  msg.requested_count = htons(CONFIG_TALKER_STREAM_SOURCES); // Announcing allocated MAC addresses
 
   ssize_t len = write(state->socket, &msg, sizeof(msg));
   if (len < 0)
@@ -88,7 +89,7 @@ void maap_send_probe(struct avtp_state_s* state)
   create_maap_msg(state, &msg, MAAP_MSG_TYPE_PROBE);
 
   memcpy(msg.requested_start_address, state->maap_db.mac, ETH_ADDR_LEN);
-  msg.requested_count = htons(0x0001); // Requesting 1 MAC address
+  msg.requested_count = htons(CONFIG_TALKER_STREAM_SOURCES); // Requesting 1 MAC address
 
   ssize_t len = write(state->socket, &msg, sizeof(msg));
   if (len < 0)
@@ -137,7 +138,7 @@ void dec_maap_probe_count(struct avtp_state_s* state)
   }
   else
   {
-    maap_state_machine(state, MAAP_EVENT_PROBE_COUNT);
+    maap_state_machine(state, MAAP_EVENT_PROBE_COUNT, NULL);
   }
 }
 
@@ -161,16 +162,33 @@ static void maap_handle_probe_timer(void* arg)
   dec_maap_probe_count(state);
 }
 
-void maap_state_machine(struct avtp_state_s* state, maap_event event)
+bool compare_mac(const u8* a, const u8 a_count, const u8* b, const u8 b_count)
+{
+  u64 a_start, b_start;
+  memcpy(&a_start, a, 6);
+  memcpy(&b_start, b, 6);
+
+  u64 a_end = a_start + a_count - 1;
+  u64 b_end = b_start + b_count - 1;
+
+  if (a_start <= b_end && b_start <= a_end)
+  {
+    return true; // Ranges overlap
+  }
+  return false; // No overlap
+}
+
+void maap_state_machine(struct avtp_state_s* state, maap_event event, struct maap_pdu_s* msg)
 {
   switch (event)
   {
   case MAAP_EVENT_BEGIN:
+  case MAAP_EVENT_RESTART:
     ESP_LOGI(TAG, "MAAP Event: BEGIN");
     if (state->maap_db.state == MAAP_STATE_INITIAL)
     {
       generate_address(state->maap_db.mac);
-      return maap_state_machine(state, MAAP_EVENT_RESERVE_ADDRESS);
+      return maap_state_machine(state, MAAP_EVENT_RESERVE_ADDRESS, NULL);
     }
     break;
   case MAAP_EVENT_RESERVE_ADDRESS:
@@ -209,6 +227,39 @@ void maap_state_machine(struct avtp_state_s* state, maap_event event)
     maap_send_announce(state, state->maap_db.mac, 1);
     state->maap_db.state = MAAP_STATE_DEFEND;
     break;
+
+  case MAAP_EVENT_RANNOUNCE:
+
+    if (state->maap_db.state == MAAP_STATE_INITIAL)
+    {
+      // Ignore announce in INITIAL state
+      break;
+    }
+
+    const bool conflict = compare_mac(msg->requested_start_address, ntohs(msg->requested_count),
+                                      state->maap_db.mac, CONFIG_TALKER_STREAM_SOURCES);
+    if (conflict)
+    {
+      ESP_LOGW(TAG, "MAAP Conflict detected from Announce message!");
+      // Stop announce timer
+      switch (state->maap_db.state)
+      {
+      case MAAP_STATE_PROBE:
+        esp_timer_stop(state->maap_db.probe_timer_handle);
+        esp_timer_delete(state->maap_db.probe_timer_handle);
+        break;
+      case MAAP_STATE_DEFEND:
+        esp_timer_stop(state->maap_db.announce_timer_handle);
+        esp_timer_delete(state->maap_db.announce_timer_handle);
+        break;
+      default:
+        break;
+      }
+      state->maap_db.state = MAAP_STATE_INITIAL;
+      // Restart allocation process
+      return maap_state_machine(state, MAAP_EVENT_RESTART, NULL);
+    }
+    break;
   case MAAP_EVENT_RELEASE:
     switch (state->maap_db.state)
     {
@@ -234,7 +285,7 @@ void maap_state_machine(struct avtp_state_s* state, maap_event event)
 
 void maap_init(struct avtp_state_s* state)
 {
-  maap_state_machine(state, MAAP_EVENT_BEGIN);
+  maap_state_machine(state, MAAP_EVENT_BEGIN, NULL);
 }
 
 void maap_net_rx(struct avtp_state_s* state, struct maap_pdu_s* msg, ssize_t len)
@@ -242,7 +293,7 @@ void maap_net_rx(struct avtp_state_s* state, struct maap_pdu_s* msg, ssize_t len
   switch (msg->message_type)
   {
   case MAAP_MSG_TYPE_ANNOUNCE:
-    handle_maap_announce(state, msg);
+    maap_state_machine(state, MAAP_EVENT_RANNOUNCE, msg);
     break;
   case MAAP_MSG_TYPE_PROBE:
     ESP_LOGI(TAG, "MAAP Probe received");
