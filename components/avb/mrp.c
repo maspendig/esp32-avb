452adf7fbb2e4bc8e816b1d4b23c03e36238173e
Maximilian Spendig <maximilian@spendig.de>
2026-01-04 18:49:06 +0100

    feat(mrp): add string conversion functions for MRP states, events, and actions

--- components/avb/mrp.c
+++ components/avb/mrp.c
@@ -4,6 +4,82 @@
 
 #include "mrp.h"
 
+char* mrp_state_to_str(mrp_state_t state)
+{
+  switch (state)
+  {
+  case MRP_VO_STATE: return "VO";
+  case MRP_VP_STATE: return "VP";
+  case MRP_VN_STATE: return "VN";
+  case MRP_AN_STATE: return "AN";
+  case MRP_AA_STATE: return "AA";
+  case MRP_QA_STATE: return "QA";
+  case MRP_LA_STATE: return "LA";
+  case MRP_AO_STATE: return "AO";
+  case MRP_QO_STATE: return "QO";
+  case MRP_AP_STATE: return "AP";
+  case MRP_QP_STATE: return "QP";
+  case MRP_LO_STATE: return "LO";
+
+  // Registrar states
+  case MRP_IN_STATE: return "IN";
+  case MRP_LV_STATE: return "LV";
+  case MRP_MT_STATE: return "MT";
+
+  default: return "UNKNOWN";
+  }
+}
+
+/* resolve protocol event states to 802.1Q-2018 10.7.1 nomenclature */
+char* mrp_event_to_str(mrp_event_t event)
+{
+  switch (event)
+  {
+  case MRP_EVENT_BEGIN: return "Begin!";
+  case MRP_EVENT_NEW: return "New!";
+  case MRP_EVENT_JOIN: return "Join!";
+  case MRP_EVENT_LV: return "Lv!";
+  case MRP_EVENT_TX: return "tx!";
+  case MRP_EVENT_TXLA: return "txLA!";
+  case MRP_EVENT_TXLAF: return "txLAF!";
+  case MRP_EVENT_R_NEW: return "rNew!";
+  case MRP_EVENT_R_JOIN_IN: return "rJoinIn!";
+  case MRP_EVENT_R_IN: return "rIn!";
+  case MRP_EVENT_R_JOIN_MT: return "rJoinMt!";
+  case MRP_EVENT_R_MT: return "rMt!";
+  case MRP_EVENT_R_LV: return "rLv!";
+  case MRP_EVENT_R_LA: return "rLa!";
+  case MRP_EVENT_FLUSH: return "Flush!";
+  case MRP_EVENT_REDECLARE: return "Re-Declare!";
+  case MRP_EVENT_PERIODIC: return "periodic!";
+  case MRP_EVENT_LEAVETIMER: return "leavetimer!";
+  case MRP_EVENT_LEAVEALLTIMER: return "leavealltimer!";
+  case MRP_EVENT_PERIODICTIMER: return "periodictimer!";
+  default: return "UNKNOWN";
+  }
+}
+
+char* mrp_action_to_str(mrp_action_t action)
+{
+  switch (action)
+  {
+  case MRP_ACTION_NONE: return "None";
+  case MRP_ACTION_NEW: return "New";
+  case MRP_ACTION_JOIN: return "Join";
+  case MRP_ACTION_LV: return "Lv";
+  case MRP_ACTION_S_N: return "sN";
+  case MRP_ACTION_S_J: return "sJ";
+  case MRP_ACTION_S_L: return "sL";
+  case MRP_ACTION_S: return "s";
+  case MRP_ACTION_S_LA: return "sLA";
+  case MRP_ACTION_PERIODIC: return "periodic";
+  case MRP_ACTION_LEAVETIMER: return "leavetimer";
+  case MRP_ACTION_LEAVEALLTIMER: return "leavealltimer";
+  case MRP_ACTION_PERIODICTIMER: return "periodictimer";
+  default: return "UNKNOWN";
+  }
+}
+
 void mrp_state_init()
 {
 }
--- components/avb/mrp.h
+++ components/avb/mrp.h
@@ -27,5 +27,45 @@ typedef enum
   MRP_MT_STATE  /* when Registrar state is empty */
 } mrp_state_t;
 
+typedef enum
+{
+  MRP_EVENT_BEGIN = 1,
+  MRP_EVENT_NEW,
+  MRP_EVENT_JOIN,
+  MRP_EVENT_LV,
+  MRP_EVENT_TX,
+  MRP_EVENT_TXLA,
+  MRP_EVENT_TXLAF,
+  MRP_EVENT_R_NEW,
+  MRP_EVENT_R_JOIN_IN,
+  MRP_EVENT_R_IN,
+  MRP_EVENT_R_JOIN_MT,
+  MRP_EVENT_R_MT,
+  MRP_EVENT_R_LV,
+  MRP_EVENT_R_LA, /* Leave All */
+  MRP_EVENT_FLUSH,
+  MRP_EVENT_REDECLARE,
+  MRP_EVENT_PERIODIC,
+  MRP_EVENT_LEAVETIMER,
+  MRP_EVENT_LEAVEALLTIMER,
+  MRP_EVENT_PERIODICTIMER,
+} mrp_event_t;
+
+typedef enum
+{
+  MRP_ACTION_NONE = 0,
+  MRP_ACTION_NEW,		/** send a New indication to MAP and the MRP application */
+  MRP_ACTION_JOIN,	/* send a Join indication to MAP and the MRP application */
+  MRP_ACTION_LV,		/* send a Lv indication to MAP and the MRP application */
+  MRP_ACTION_S_N,		/* send a New message */
+  MRP_ACTION_S_J,		/* send a JoinIn or JoinMt message */
+  MRP_ACTION_S_L,		/* send a Lv message */
+  MRP_ACTION_S,		/**< send a In or Empty message */
+  MRP_ACTION_S_LA,		/**< send a Leave All message */
+  MRP_ACTION_PERIODIC,	/**< Periodic transmission event */
+  MRP_ACTION_LEAVETIMER,	/**< Leave All period timer */
+  MRP_ACTION_LEAVEALLTIMER,	/**< Leave All timer */
+  MRP_ACTION_PERIODICTIMER	/**< Periodic transmission timer */
+} mrp_action_t;
 
 #endif //ETHERNET_PTP_MRP_H
